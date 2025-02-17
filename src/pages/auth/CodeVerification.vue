2aec98a9f6e836027f036896350425d61dc1cb60
Mohammad-Nadr <amoo.nadr81@gmail.com>
2025-02-17 17:45:13 +0330

    working

--- src/pages/auth/CodeVerification.vue
+++ src/pages/auth/CodeVerification.vue
@@ -1,37 +1,40 @@
 <script setup lang="ts">
 import AuthCodeVerification from '@/components/authForms/AuthCodeVerification.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n(); // دسترسی به تابع t برای ترجمه
 </script>
 
 <template>
   <v-row no-gutters>
     <!---Code verification Part-->
     <v-col
-      cols="12"
-      class="d-flex align-center"
+        cols="12"
+        class="d-flex align-center"
     >
       <v-container>
         <div
-          class="d-flex align-center justify-center"
-          style="min-height: calc(100vh - 148px)"
+            class="d-flex align-center justify-center"
+            style="min-height: calc(100vh - 148px)"
         >
           <v-row justify="center">
             <v-col
-              cols="12"
-              lg="10"
-              xl="6"
-              md="7"
+                cols="12"
+                lg="10"
+                xl="6"
+                md="7"
             >
               <v-card
-                elevation="0"
-                class="loginBox"
+                  elevation="0"
+                  class="loginBox"
               >
                 <v-card elevation="24">
                   <v-card-text class="pa-sm-9 pa-6">
                     <h3 class="text-h3 mb-2">
-                      Enter Verification Code
+                      {{ t('enterVerificationCode') }}
                     </h3>
                     <p class="text-h6 text-lightText">
-                      We send you on mail.
+                      {{ t('verificationCodeMessage') }}
                     </p>
 
                     <!---Code verification Form-->
@@ -45,9 +48,9 @@ import AuthCodeVerification from '@/components/authForms/AuthCodeVerification.vu
         </div>
       </v-container>
     </v-col>
-
   </v-row>
 </template>
+
 <style lang="scss">
 .loginBox {
   max-width: 475px;
